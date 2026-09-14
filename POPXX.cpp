50fbe935c797185bae967b9c69ec85c55f9b86b6
Dhananjay Bankar <183342812+Dhananjay9834@users.noreply.github.com>
2026-09-14 19:52:22 +0530

    Add files via upload

---
+++ POPXX.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+struct Arithematic
+{
+    int No1;
+    int No2;
+};
+
+int main()
+{
+    Arithematic aobj1;
+    
+    aobj1.No1 = 0;
+    aobj1.No2 = 0;
+
+    aobj1.No1 = 10;
+    aobj1.No2 = 21;
+
+    
+
+    return 0;
+}
