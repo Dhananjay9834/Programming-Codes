a72fb88d61be7c2750dd032d37fedaa206dc23a1
Dhananjay Bankar <183342812+Dhananjay9834@users.noreply.github.com>
2026-09-18 22:50:34 +0530

    Add files via upload

---
+++ Java Language/Selection1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Selection1
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int No = 0;
+
+        System.out.println("Enter Number : ");
+        No = sobj.nextInt();
+
+        if((No % 2) == 0)
+        {
+            System.out.println("Number is EVEN");
+        }
+        else
+        {
+            System.out.println("Number is ODD");
+        }
+
+    }
+}
