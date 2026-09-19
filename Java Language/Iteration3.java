22e644a0916bc05624c6b0440a562a7bc52d3250
Dhananjay Bankar <183342812+Dhananjay9834@users.noreply.github.com>
2026-09-19 23:47:23 +0530

    Add files via upload

---
+++ Java Language/Iteration3.java
@@ -0,0 +1,13 @@
+class Iteration3
+{
+    public static void main(String A[])
+    {
+        int i = 1; //1
+    
+        while(i<=4) //2
+        {
+            System.out.println("Jay Ganesh..."); // 4
+            i++;                                   //3
+        }
+    }
+}
